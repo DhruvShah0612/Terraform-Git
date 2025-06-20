c7da233f23968a9aa0a9e584364f639d9c7511ba
Dhruvshah0612 <dhruvshah0612@gmail.com>
2025-06-21 00:36:41 +0530

    Fix: removed recursive module call from modules/s3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -3,24 +3,4 @@ resource "aws_s3_bucket" "this" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.this.id
-
-  versioning_configuration {
-    status = var.versioning.enabled ? "Enabled" : "Suspended"
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "this" {
-  bucket = aws_s3_bucket.this.id
-
-  rule {
-    object_ownership = var.object_ownership
-  }
-}
-
 variable "bucket" {}
-variable "versioning" {
-  type = object({ enabled = bool })
-}
-variable "object_ownership" {}
