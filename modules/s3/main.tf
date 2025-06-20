afc396ccb548de69f7def252910b7ccbf4069db9
Dhruvshah0612 <dhruvshah0612@gmail.com>
2025-06-21 00:41:51 +0530

    ✅ Fix s3 module with all required inputs

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,6 +1,29 @@
+variable "bucket" {}
+variable "acl" {}
+variable "control_object_ownership" {}
+variable "object_ownership" {}
+variable "versioning" {}
+variable "tags" {}
+
 resource "aws_s3_bucket" "this" {
-  bucket = var.bucket
+  bucket        = var.bucket
   force_destroy = true
+
+  tags = var.tags
 }
 
-variable "bucket" {}
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    object_ownership = var.object_ownership
+  }
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = var.versioning.enabled ? "Enabled" : "Suspended"
+  }
+}
