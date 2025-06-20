22549d0968e4ee492eb7c7c6bee32deffef6211c
Dhruvshah0612 <dhruvshah0612@gmail.com>
2025-06-20 19:42:06 +0530

    ✨ Added Security Group for web access

---
+++ security-group.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "web-sg"
+    Owner = "Dhruv Shah"
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
